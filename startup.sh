ceb23d78a5f983a148807ba0172570e9085b8177
Per Mosegaard <permosegaard@googlemail.com>
2016-05-11 16:48:23 +0100

    Update startup.sh
    minor changes

--- startup.sh
+++ startup.sh
@@ -2,14 +2,6 @@
 
 ip route change default via 172.17.42.254
 
-if [ ! -x "/home/steam/steamcmd/steamcmd.sh" ]
-then
-	mkdir -p /home/steam/steamcmd/
-	curl -s "https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz" | tar -vzx -C "/home/steam/steamcmd/"
-fi
-
-/home/steam/steamcmd/steamcmd.sh +login $STEAM_USERNAME $STEAM_PASSWORD +force_install_dir /home/steam/app +app_update 294420 +quit
-
 function _shutdown() {
   echo "Shutting down gracefully.."
   { echo "saveworld"; sleep 1; echo "shutdown"; sleep 1; } | telnet 127.0.0.1 8081
