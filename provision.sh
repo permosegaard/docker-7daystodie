d9f88ef2d164c32bee3874eb2807b7f12c52c1b4
Per Mosegaard <permosegaard@googlemail.com>
2016-05-11 16:51:09 +0100

    Create provision.sh
    minor changes

---
+++ provision.sh
@@ -0,0 +1,15 @@
+export DEBIAN_FRONTEND=noninteractive
+apt-get update && apt-get install -qy iproute2 
+apt-get install -qy apt-utils ca-certificates lib32gcc1 net-tools lib32stdc++6 lib32z1 lib32z1-dev curl telnet
+apt-get clean && rm -Rf /var/lib/apt/lists/*
+
+useradd steam
+mkdir -p /home/steam/app /home/steam/steamcmd
+chown -R steam:steam /home/steam
+
+mkdir -p /home/steam/steamcmd/
+curl -s "https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz" | tar -vzx -C "/home/steam/steamcmd/"
+
+/home/steam/steamcmd/steamcmd.sh +login $STEAM_USERNAME $STEAM_PASSWORD +force_install_dir /home/steam/app +app_update 294420 +quit
+
+apt-get clean && rm -Rf /var/lib/apt/lists/*
