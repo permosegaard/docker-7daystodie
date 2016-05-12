7fce18370d91690f4b61698c0dbb98957f616cc8
Per Mosegaard <permosegaard@googlemail.com>
2016-05-12 23:27:27 +0100

    Update startup.sh
    minor change

--- startup.sh
+++ startup.sh
@@ -2,6 +2,8 @@
 
 ip route change default via 172.17.42.254
 
+sed -i "s%^  <property name=\"ServerPort\" 				value=\"26900\"/>%  <property name="ServerPort" 				value=\"${PORT_26900}\"/>%" /server/serverconfig.xml
+
 function _shutdown() {
   echo "Shutting down gracefully.."
   { echo "saveworld"; sleep 1; echo "shutdown"; sleep 1; } | telnet 127.0.0.1 8081
