b0059cb3a86a4601a337446dde1f99f37abd74ba
Per Mosegaard <permosegaard@googlemail.com>
2016-05-13 11:54:47 +0100

    Update provision.sh
    minor changes

--- provision.sh
+++ provision.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 export DEBIAN_FRONTEND=noninteractive
+
+echo "Europe/London" | sudo tee /etc/timezone && sudo dpkg-reconfigure tzdata
 apt-get update && apt-get install -qy iproute2 
+
 apt-get install -qy apt-utils ca-certificates lib32gcc1 net-tools lib32stdc++6 lib32z1 lib32z1-dev curl telnet
 
 mkdir -p /root/steamcmd /server
