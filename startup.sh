ee745ba44a40256a0caa73c8729ef9a74f9b6a2f
Per Mosegaard <permosegaard@googlemail.com>
2016-05-14 21:24:13 +0100

    Update startup.sh
    minor update

--- startup.sh
+++ startup.sh
@@ -4,7 +4,8 @@ ip route change default via 172.17.42.254
 
 ulimit -n 2048
 
-mkdir /server && cp -Ra /seed/${CONTAINER_TYPE}/ /server/
+echo cp -Ra /seed/${CONTAINER_TYPE}/ /server/
+cp -Ra /seed/${CONTAINER_TYPE}/ /server/
 /root/steamcmd/steamcmd.sh +login anonymous +force_install_dir /server +app_update 294420 +quit
 
 sed -i "s%^  <property name=\"ServerPort\"[ \t]*value=\"[0-9]*\"/>%  <property name=\"ServerPort\" value=\"${PORT_26900}\"/>%" /server/serverconfig.xml
