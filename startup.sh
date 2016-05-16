39f77cc722b362b683e822b588ac5c04b7486841
Per Mosegaard <permosegaard@googlemail.com>
2016-05-16 15:47:30 +0100

    Update startup.sh
    minor update

--- startup.sh
+++ startup.sh
@@ -2,24 +2,24 @@
 
 ip route change default via 172.17.42.254
 
-# uncomment to update seed - when d/l complete, shell in and rsync /server/* and /root/steamcmd/* to host /seed/$type/{game,steamcmd}
-# curl -s "https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz" | tar -vzx -C "/root/steamcmd/"
-# if [ -z "${STEAM_USER}" ]; then CREDENTIALS="anonymous"; else CREDENTIALS="${STEAM_USERNAME} ${STEAM_PASSWORD}"; fi
-# /root/steamcmd/steamcmd.sh +login $CREDENTIALS +force_install_dir /server +app_update 294420 validate +quit
-# read && exit
+# start seed update section
+# shell in and rsync /server/* and /root/steamcmd/* to host /seed/$type/{game,steamcmd}
+curl -s "https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz" | tar -vzx -C "/root/steamcmd/"
+if [ -z "${STEAM_USER}" ]; then CREDENTIALS="anonymous"; else CREDENTIALS="${STEAM_USERNAME} ${STEAM_PASSWORD}"; fi
+/root/steamcmd/steamcmd.sh +login $CREDENTIALS +force_install_dir /server +app_update 294420 validate +quit
+echo && echo "update complete, pausing..." && read && exit
+# end seed update section
 
 if [ "$( find /server/ -type f | wc -l )" -lt "1" ]
 then
   echo "copying seed across... this may take some time depending on the game size"
   rm -Rf /server/* /root/steamcmd/*
-  #cp -Rf /seed/${CONTAINER_TYPE}/game/* /server/ # change to hard/soft links
-  #cp -Rf /seed/${CONTAINER_TYPE}/steamcmd/* /root/steamcmd/ # change to hard/soft links
+  cp -Rf /seed/${CONTAINER_TYPE}/game/* /server/ # change to hard/soft links
+  cp -Rf /seed/${CONTAINER_TYPE}/steamcmd/* /root/steamcmd/ # change to hard/soft links
 fi
 
 if [ -z "${STEAM_USER}" ]; then CREDENTIALS="anonymous"; else CREDENTIALS="${STEAM_USERNAME} ${STEAM_PASSWORD}"; fi
 /root/steamcmd/steamcmd.sh +login $CREDENTIALS +force_install_dir /server +app_update 294420 validate +quit
-
-read
 
 sed -i "s%^  <property name=\"ServerPort\"[ \t]*value=\"[0-9]*\"/>%  <property name=\"ServerPort\" value=\"${PORT_26900}\"/>%" /server/serverconfig.xml
 sed -i "s%^  <property name=\"ServerName\"[ \t]*value=\"My Game Host\"/>%  <property name=\"ServerName\" value=\"${CONTAINER_NAME}\"/>%" /server/serverconfig.xml
