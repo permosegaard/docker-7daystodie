1e1b8b82f6420fbdf29262d50e412f916cf2252f
Per Mosegaard <permosegaard@googlemail.com>
2016-05-16 15:39:46 +0100

    Update provision.sh
    minor update

--- provision.sh
+++ provision.sh
@@ -9,6 +9,4 @@ apt-get install -qy apt-utils ca-certificates lib32gcc1 net-tools lib32stdc++6 l
 
 mkdir -p /root/steamcmd /server
 
-curl -s "https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz" | tar -vzx -C "/root/steamcmd/"
-
 apt-get clean && rm -Rf /var/lib/apt/lists/*
