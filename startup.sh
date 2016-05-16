49ee260aa09970c07ace04ff1a63a2c87e168813
Per Mosegaard <permosegaard@googlemail.com>
2016-05-16 15:06:29 +0100

    Update startup.sh
    minor update

--- startup.sh
+++ startup.sh
@@ -5,7 +5,8 @@ ip route change default via 172.17.42.254
 ulimit -n 2048
 
 if [ "$( find /server/ -type f | wc -l )" -lt "1" ]; then echo "copying seed across... this may take some time depending on the game size"; time cp -Rs /seed/${CONTAINER_TYPE}/ /server/; fi
-/root/steamcmd/steamcmd.sh +login anonymous +force_install_dir /server +app_update 294420 validate +quit
+if [  -z "${STEAM_USER}"  ]; CREDENTIALS="anonymous"; else CREDENTIALS="${STEAM_USERNAME} ${STEAM_PASSWORD}"; fi
+/root/steamcmd/steamcmd.sh +login $CREDENTIALS +force_install_dir /server +app_update 294420 validate +quit
 
 sed -i "s%^  <property name=\"ServerPort\"[ \t]*value=\"[0-9]*\"/>%  <property name=\"ServerPort\" value=\"${PORT_26900}\"/>%" /server/serverconfig.xml
 sed -i "s%^  <property name=\"ServerName\"[ \t]*value=\"My Game Host\"/>%  <property name=\"ServerName\" value=\"${CONTAINER_NAME}\"/>%" /server/serverconfig.xml
