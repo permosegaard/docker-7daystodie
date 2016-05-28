07cb8f74b07ae5ac170d63fb78ea11250c279aaa
Per Mosegaard <permosegaard@googlemail.com>
2016-05-28 15:27:31 +0100

    Update provision.sh
    minor update

--- provision.sh
+++ provision.sh
@@ -5,6 +5,6 @@ export DEBIAN_FRONTEND=noninteractive
 echo "Europe/London" > /etc/timezone && dpkg-reconfigure tzdata
 apt-get update && apt-get install -qy iproute2 
 
-apt-get install -qy unionfs-fuse apt-utils ca-certificates lib32gcc1 net-tools lib32stdc++6 lib32z1 lib32z1-dev curl telnet
+apt-get install -qy apt-utils ca-certificates lib32gcc1 net-tools lib32stdc++6 lib32z1 lib32z1-dev curl telnet
 
 apt-get clean && rm -Rf /var/lib/apt/lists/*
