c49984ab85c1845118950be96c838084e93480d6
Per Mosegaard <permosegaard@googlemail.com>
2016-05-28 12:54:04 +0100

    Update provision.sh
    minor update

--- provision.sh
+++ provision.sh
@@ -7,6 +7,4 @@ apt-get update && apt-get install -qy iproute2
 
 apt-get install -qy unionfs-fuse apt-utils ca-certificates lib32gcc1 net-tools lib32stdc++6 lib32z1 lib32z1-dev curl telnet
 
-mkdir -p {,/overlay}/root/Steam {,/overlay}/root/steamcmd {,/overlay}/server
-
 apt-get clean && rm -Rf /var/lib/apt/lists/*
